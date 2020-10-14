6cb8b781d98375143131f83d6fb078b6d41aaf1f
Mylan Connolly <mconnolly@iotrak.com>
2020-10-14 10:06:13 -0400

    Add documentation and minor fixes to reader

--- reader.go
+++ reader.go
@@ -28,11 +28,23 @@ func NewReader(reader io.Reader) *Reader {
 
 type MessageFunc func(msg *Message) error
 
+// EachMessage is used to create a bit of a friendlier API for reading messages
+// if all you're doing is (for example) importing the messages' data into a
+// database or something. Each message is passed into the MessageFunc provided
+// and executed in order. The downside of this implementation is that it will
+// not happen concurrently, which could have some performance ramifications.
+//
+// Errors returned from this will not include io.EOF, so when you're done
+// processing the work, only "real" errors are returned here, such as errors
+// parsing the HL7 data and errors reading from the input io.Reader.
 func (r *Reader) EachMessage(fn MessageFunc) error {
 	for {
 		msg, err := r.ReadMessage()
 
 		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
 			return err
 		}
 		if err = fn(msg); err != nil {
@@ -55,6 +67,8 @@ func (r *Reader) ReadMessage() (*Message, error) {
 
 		if err == io.EOF {
 			break
+		} else if err != nil {
+			return nil, err
 		}
 		// Skip all characters that don't look like they're the beginning of a
 		// message until we start storing bytes in the byte slice. This helps us
--- reader_test.go
+++ reader_test.go
@@ -2,7 +2,9 @@ package hl7
 
 import (
 	"bytes"
+	"errors"
 	"testing"
+	"testing/iotest"
 )
 
 func TestReaderReadMessage(t *testing.T) {
@@ -42,4 +44,79 @@ func TestReaderReadMessage(t *testing.T) {
 			}
 		})
 	}
+
+	t.Run("read error is propagated", func(t *testing.T) {
+		buf := bytes.NewBufferString("MSH|....")
+		reader := NewReader(iotest.TimeoutReader(buf))
+		_, err := reader.ReadMessage()
+
+		if err == nil {
+			t.Fatal("Did not get error")
+		}
+	})
+}
+
+func TestReaderEachMessage(t *testing.T) {
+	tests := []struct {
+		name  string
+		data  string
+		count int
+	}{
+		{"empty", "", 0},
+		{"one message", "MSH|....", 1},
+		{"two messages (CR)", "MSH|....\rMSH|.....", 2},
+		{"two messages (CRLF)", "MSH|....\r\nMSH|.....", 2},
+		{"two messages (FF)", "MSH|....\fMSH|.....", 2},
+		{"one message with trailing delimiter (CR)", "MSH|....\r", 1},
+		{"one message with trailing delimiter (CRLF)", "MSH|....\r\n", 1},
+		{"one message with trailing delimiter (FF)", "MSH|....\f", 1},
+		{"two messages with trailing delimiter (CR)", "MSH|....\rMSH|.....\r", 2},
+		{"two messages with trailing delimiter (CRLF)", "MSH|....\r\nMSH|.....\r\n", 2},
+		{"two messages with trailing delimiter (FF)", "MSH|....\fMSH|.....\f", 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			buf := bytes.NewBufferString(tt.data)
+			reader := NewReader(buf)
+			i := 0
+
+			err := reader.EachMessage(func(msg *Message) error {
+				i++
+				return nil
+			})
+
+			if i != tt.count {
+				t.Fatalf("Got unexpected number of messages (%d), wanted %d", i, tt.count)
+			}
+			if err != nil {
+				t.Fatalf("Got error reading messages: %v", err)
+			}
+		})
+	}
+
+	t.Run("read error is propagated", func(t *testing.T) {
+		buf := bytes.NewBufferString("MSH|....")
+		reader := NewReader(iotest.TimeoutReader(buf))
+
+		err := reader.EachMessage(func(msg *Message) error {
+			return nil
+		})
+
+		if err == nil {
+			t.Fatal("Did not get error")
+		}
+	})
+
+	t.Run("error is propagated", func(t *testing.T) {
+		buf := bytes.NewBufferString("MSH|....")
+		reader := NewReader(buf)
+
+		err := reader.EachMessage(func(msg *Message) error {
+			return errors.New("foo")
+		})
+
+		if err == nil {
+			t.Fatal("Did not get error")
+		}
+	})
 }
